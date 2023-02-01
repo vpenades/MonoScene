1edbf858fac1256f598ed94f2c3c36e72b192232
vpenades <5433822+vpenades@users.noreply.github.com>
2023-02-01 13:08:42 +0100

    Added iOS demo [WIP]

---
+++ demos/Demo5.AppleiOS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Foundation;
+
+using UIKit;
+
+namespace Demo5.AppleiOS
+{
+    [Register("AppDelegate")]
+    internal class Program : UIApplicationDelegate
+    {
+        private static Game1 game;
+
+        internal static void RunGame()
+        {
+            game = new Game1();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
